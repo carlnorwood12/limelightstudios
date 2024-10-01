85745276cfbfa120013c73631b1d958ba34b42fe
Carl <94581214+carlnorwood12@users.noreply.github.com>
2024-10-01 15:23:42 +0100

    Add files via upload

--- scroll.js
+++ scroll.js
@@ -11,8 +11,10 @@ tl.from(".uk-accordion", {
   scale: 0.95,
   opacity: 0,
   filter: "grayscale(100%)",
+  filter: "blur(10px)",
   duration: 1,
 });
+
 let tl2 = gsap.timeline({
   scrollTrigger: {
     trigger: ".container",
@@ -26,8 +28,10 @@ tl2.from(".container", {
   scale: 0.95,
   opacity: 0,
   filter: "grayscale(100%)",
+  filter: "blur(5px)",
   duration: 1,
 });
+
 let tl3 = gsap.timeline({
   scrollTrigger: {
     trigger: ".yoo .swiper-slide",
@@ -37,12 +41,22 @@ let tl3 = gsap.timeline({
     id: "scrub",
   },
 });
-tl3.from(" .yo .swiper-slide", {
+
+tl3.from(" .yoo .swiper-slide", {
   stagger: 0.125,
+  filter: "blur(5px)",
   y: 50,
   opacity: 0,
   duration: 1,
 });
+tl3.to(".yoo .swiper-slide", {
+  stagger: 0.125,
+  filter: "blur(0)",
+  y: 0,
+  opacity: 1,
+  duration: 1,
+});
+
 let tl4 = gsap.timeline({
   scrollTrigger: {
     trigger: ".content-suggested .swiper-slide",
@@ -52,12 +66,22 @@ let tl4 = gsap.timeline({
     id: "scrub",
   },
 });
+
 tl4.from(".content-suggested .swiper-slide", {
   stagger: 0.125,
+  filter: "blur(5px)",
   y: 50,
   opacity: 0,
   duration: 1,
 });
+tl4.to(".content-suggested .swiper-slide", {
+  stagger: 0.125,
+  filter: "blur(0)",
+  y: 0,
+  opacity: 1,
+  duration: 1,
+});
+
 let tl5 = gsap.timeline({
   scrollTrigger: {
     trigger: ".content-releases .swiper-slide",
@@ -71,5 +95,7 @@ tl5.from(".content-releases .swiper-slide", {
   stagger: 0.125,
   y: 50,
   opacity: 0,
+  filter: "blur(5px)",
   duration: 1,
 });
+gsap.fromTo(".button", { scale: 0.8, opacity: 0, stagger: 0.2 }, { scale: 1, opacity: 1, stagger: 0.2, duration: 1 });
