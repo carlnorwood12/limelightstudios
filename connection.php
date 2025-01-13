ec9ad94628a54fc6fc91ffc17f413cc3af262109
R2 <94581214+carlnorwood12@users.noreply.github.com>
2025-01-13 00:36:59 +0000

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,11 +1,14 @@
 <?php
 
-$username = "root";
-$password = "test123";
-$hostname = "127.0.0.1";
-$database = "limelight_cinemas";
+$username = "phpmyadmin";  // Your MySQL user
+$password = "Str0ngPassword!2025";  // Your MySQL password
+$hostname = "192.168.1.100";  // Replace with your MySQL server's IP address
+$database = "phpmyadmin";  // The database you want to connect to
 
+// Create a connection to the MySQL server
 $dbhandle = mysqli_connect($hostname, $username, $password) or die("Could not connect to database");
+
+// Select the database
 $selected = mysqli_select_db($dbhandle, $database) or die("Could not select database");
 
 ?>
