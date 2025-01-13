389cdf7e465c5cd060977f57a5996c699044d638
R2 <94581214+carlnorwood12@users.noreply.github.com>
2025-01-13 01:14:50 +0000

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,14 +1,16 @@
 <?php
-
 $username = "phpmyadmin";  // Your MySQL user
 $password = "Str0ngPassword!2025";  // Your MySQL password
-$hostname = "192.168.1.100";  // Replace with your MySQL server's IP address
+$hostname = "127.0.0.1";  // Use localhost
 $database = "phpmyadmin";  // The database you want to connect to
 
+// Specify the socket path
+$socket = '/var/run/mysqld/mysqld.sock';  // Adjust this path if needed
+
 // Create a connection to the MySQL server
-$dbhandle = mysqli_connect($hostname, $username, $password) or die("Could not connect to database");
+$dbhandle = mysqli_connect($hostname, $username, $password, $database, null, $socket)
+or die("Could not connect to database");
 
 // Select the database
 $selected = mysqli_select_db($dbhandle, $database) or die("Could not select database");
-
 ?>
