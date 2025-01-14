151b584a8de916ef1701ade91b674eeb1bf1bd6e
Your Name <carlnorwood12@gmail.com>
2025-01-14 17:04:21 +0000

    Resolve merge conflict in connection.php

--- connection.php
+++ connection.php
@@ -1,14 +1,13 @@
 <?php
-$username = "phpmyadmin";  // Your MySQL user
-$password = "Str0ngPassword!2025";  // Your MySQL password
+$username = "root";  // Your MySQL user
+$password = "your_new_password";  // Your MySQL password
 $hostname = "127.0.0.1";  // Use localhost
-$database = "phpmyadmin";  // The database you want to connect to
+$database = "limelightcinema";  // The database you want to connect to
 
 // Specify the socket path
-$socket = '/var/run/mysqld/mysqld.sock';  // Adjust this path if needed
 
 // Create a connection to the MySQL server
-$dbhandle = mysqli_connect($hostname, $username, $password, $database, null, $socket)
+$dbhandle = mysqli_connect($hostname, $username, $password, $database)
 or die("Could not connect to database");
 
 // Select the database
